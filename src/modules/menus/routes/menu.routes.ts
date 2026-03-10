c953fa02824621473bb7becb8099740c62ffca4e
Murilo Frizanco <murilo140891@gmail.com>
2026-03-10 14:10:38 -0300

    feat: rename ecommerceprodutos to ecommerce-produtos and implement dynamic screen titles using views.json

--- src/modules/menus/routes/menu.routes.ts
+++ src/modules/menus/routes/menu.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express'
 import menus from '../menus.json'
+import views from '../views.json'
 
 export const menuRoutes = Router()
 
 menuRoutes.get('/', (req, res) => {
     return res.json(menus)
+})
+
+menuRoutes.get('/views', (req, res) => {
+    return res.json(views)
 })
